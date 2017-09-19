2b3ffc70c4ba6bd994fd6651ba86aa3a73457b7a
vjay49 <rudraraju.vijay5@gmail.com>
2017-09-18 20:31:05 -0400

    Merge branches 'Gyro_NavX' and 'master' of https://github.com/FTC-9073-Knightrix/2017-Knightrix into Gyro_NavX Started to program the gyro navX # Conflicts: #	gradle/wrapper/gradle-wrapper.properties

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro_NavX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro_NavX.java
@@ -1,8 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by Vijay on 9/18/2017.
  */
+@TeleOp(name = "Tele Op")
+public class Gyro_NavX extends Telemetry {
+    public void init() {
+        teleOp = true;
+    }
 
-public class Gyro_NavX {
+    public void loop() {
+        updateTelemetry();
+        telemetry.addLine("Angular orientation rotation axes: " + String.valueOf(Nav1.getAngularOrientationAxes()));
+        telemetry.addLine("angular velocity axes: " + String.valueOf(Nav1.getAngularVelocityAxes()));
+
+    }
+
+
 }
+
