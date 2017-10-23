146b0ff6df40d3b9fb0ce01562186fe68a2d7ef0
Ignacio Bravo <ibravo@hotmail.com>
2017-10-23 19:49:25 -0400

    Wheels now working ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHardwareMap.java
@@ -25,9 +25,9 @@ public abstract class TestHardwareMap extends OpMode {
     public void init(){
 
         LeftFrontDrive = hardwareMap.dcMotor.get("LF");
-        LeftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        LeftFrontDrive.setDirection(DcMotor.Direction.FORWARD); //was reverse
         LeftBackDrive = hardwareMap.dcMotor.get("LB");
-        LeftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        LeftBackDrive.setDirection(DcMotor.Direction.FORWARD); //was reverse
         RightFrontDrive = hardwareMap.dcMotor.get("RF");
         RightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         RightBackDrive = hardwareMap.dcMotor.get("RB");
@@ -55,13 +55,12 @@ public abstract class TestHardwareMap extends OpMode {
     void mech_move (float myangle, float mypower, float myrot){
         if (LeftFrontDrive !=null && LeftBackDrive != null && RightFrontDrive != null && RightBackDrive != null ) {
 
-                LeftFrontDrive.setPower(Range.clip(-myrot +  (mypower * ((-Math.sin((myangle + 45) / 180 * 3.141592)))),-1,1));
-                LeftBackDrive.setPower(Range.clip( -myrot +  (mypower * ((-Math.sin((myangle + 135) / 180 * 3.141592)))),-1,1));
-                RightFrontDrive.setPower(Range.clip(myrot +  (mypower * ((-Math.sin((myangle + 45) / 180 * 3.141592)))),-1,1));
-                RightBackDrive.setPower(Range.clip( myrot +  (mypower * ((-Math.sin((myangle + 135) / 180 * 3.141592)))),-1,1));
+                LeftFrontDrive.setPower(Range.clip( myrot +  (-mypower * ((-Math.sin((myangle + 45) / 180 * 3.141592)))),-1,1));
+                LeftBackDrive.setPower(Range.clip(  myrot +  (-mypower * ((-Math.sin((myangle + 135) / 180 * 3.141592)))),-1,1));
+                RightFrontDrive.setPower(Range.clip(myrot +  (mypower * ((-Math.sin((myangle + 135) / 180 * 3.141592)))),-1,1));
+                RightBackDrive.setPower(Range.clip( myrot +  (mypower * ((-Math.sin((myangle + 45) / 180 * 3.141592)))),-1,1));
 
         }
-
     }
 
     
