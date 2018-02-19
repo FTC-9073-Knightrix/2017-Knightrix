f6a77322087c8ef135c40543b3a2d0c4cf2c7287
Ignacio Bravo <ibravo@hotmail.com>
2018-02-19 04:38:14 -0500

    Added Autonomous steps and code formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueRight.java
@@ -12,13 +12,62 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
  */
 @Autonomous(name = "NEWBLUE_RIGHT")
 public class AutoBlueRight extends NewHardwareMap {
+
+    /* --------------------------------------------------------------------------
+    * Code to run ONCE when the driver hits PLAY
+       --------------------------------------------------------------------------
+    */
     @Override
     public void start() {
         super.start();
     }
 
+
+    /* --------------------------------------------------------------------------
+    * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+       --------------------------------------------------------------------------
+    */
     @Override
     public void loop() {
+
+        // --------------- DESCRIPTION --------------------------
+        // Blue Middle
+        // Autonomous for Right side (Blue), middle position
+        // ------------------ START -----------------------------
+        // Code:
+        // 1. Start moving arm to the side
+        // 2. Init Variables:
+        //      a.Calculate starting angle
+        //      b.Read Vuforia
+        //      c.Encoders position
+        // 3. Lower the color sensor
+        // 4. Read the color
+        // 5. Stop the arm movement (from #1)
+        // 6. If correct color in back, move forward (Step #8)
+        // 7.   elseIf wrong color in back, tilt and tilt back
+        // 8. Enable range sensor; enable gyro
+        // 9. Go forward (using gyro) until start of wall (range < 5 cm)
+        // 10. lift color sensor servo
+        // 11. Go forward until encoders reach desired Vuforia position
+        // 12. Turn 90 degrees
+        // 13. Deploy plate
+        // 14. go forwards with sweepers ON
+        //      a. for a distance, or
+        //      b. until optical distance sensor sees block in the plate
+        // 15. go backwards same amount of encoder positions
+        // 16. deploy plate
+        // ------------------  END  -----------------------------
+
+
+        // ######################################################
+        // ##              VARIABLES                           ##
+        // ######################################################
+        //
+        // --------------- DESCRIPTION --------------------------
+        // Update Variables
+        // Update all variables for every loop
+        // ------------------ START -----------------------------
+        // Updates Gyro values
         Orientation orientation = navxGyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
 
         // Get position of the 4 encoders
@@ -33,7 +82,14 @@ public class AutoBlueRight extends NewHardwareMap {
         double xPos = ((lfEnc + rbEnc) - (rfEnc + lbEnc))*1/4.0;
         double yPos = (lfEnc + lbEnc + rfEnc + rbEnc)*1/4.0;
         double rotPos = ((lfEnc + lbEnc) - (rfEnc + rbEnc))*1/4.0;
+        // ------------------  END  -----------------------------
+
+
 */
+        // --------------- DESCRIPTION --------------------------
+        // Title
+        // Description
+        // ------------------ START -----------------------------
         if (state == 0) {
             timer = (float) getRuntime();  // Sets timer = accumulated time
             angle = orientation.firstAngle;
@@ -47,6 +103,8 @@ public class AutoBlueRight extends NewHardwareMap {
                 timer = (float) getRuntime();
             }
         }
+        // ------------------  END  -----------------------------
+
         //  Up Claw for time. Then stop motor
         else if (state == 2) {
             timer2 = (float) getRuntime() - timer;   // Calculates difference between current stage and accumulated timer
